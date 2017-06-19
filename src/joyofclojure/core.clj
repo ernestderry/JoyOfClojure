cf6fe3d24addb4f72a019c9e7b3332830acdf883
ederr <ernest.derry@travisperkins.co.uk>
2017-06-19 13:38:14 +0100

    added debugging to qsort algorithm

--- src/joyofclojure/core.clj
+++ src/joyofclojure/core.clj
@@ -6,19 +6,37 @@
 
 (defn sort-parts [work]
   (lazy-seq
-    (loop [[part & parts] work]
-      (if-let [[pivot & xs] (seq part)]
-        (let [smaller? #(< % pivot)]
-          (recur (list*
-                   (filter smaller? xs)
-                   pivot
-                   (remove smaller? xs)
-                   parts)))
-        (when-let [[x & parts] parts]
-          (cons x (sort-parts parts)))))))
+    (do
+      (println (str "work " work))
+      (loop [[part & parts] work]
+        (do
+          (println (str "seq part" (seq part)))
+          (if-let [[pivot & xs] (seq part)]
+            (do
+              (println (str "pivot " pivot " xs " xs))
+              (let [smaller? #(< % pivot)]
+                (do 
+                  (println (str "recur " (list*
+                       (filter smaller? xs)
+                       pivot
+                       (remove smaller? xs)
+                       parts)))
+                  (recur (list*
+                       (filter smaller? xs)
+                       pivot
+                       (remove smaller? xs)
+                       parts)))))
+            (do
+              (println (str "when-let parts " parts)
+              (when-let [[x & parts] parts]
+                (do
+                  (println (str "cons " (cons x (sort-parts parts))))
+                  (cons x (sort-parts parts))))))))))))
 
 (defn qsort [xs]
   (sort-parts (list xs)))
 
 (defn rand-ints [n]
   (take n (repeatedly #(rand-int n))))
+
+(qsort [4 0 6 2 0 1 9])
